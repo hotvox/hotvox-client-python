e1d57bc6f59315f7ad8c09debf964aa1b1c00e62
numemac <numemac@users.noreply.github.com>
2024-07-28 18:59:19 +0000

    ✅ Ready to clone and code.

---
+++ hotvox_client_python/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for hotvox_client_python."""
+
+from hotvox_client_python.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from hotvox_client_python.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "hotvox_client_python"
